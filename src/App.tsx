7be04a943aa9a67c937d1fb7caab225f0b12f329
Manuel Krivoy <krivoymanuel@gmail.com>
2024-09-26 20:05:20 -0300

    actualizo form victoria

--- src/App.tsx
+++ src/App.tsx
@@ -49,70 +49,72 @@ function App() {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-purple-500 to-blue-500">
-      {letras.join("") === palabra ? ( // Si las letras adivinadas coinciden con la palabra completa
-        <div>
-          <p className="text-green-500 text-center text-2xl font-bold">¡Ganaste!</p>
-          <button
-            className={
-              "w-full font-bold py-2 rounded-md transition-all duration-300 ease-in-out shadow-lg transform bg-blue-500 hover:bg-blue-600 text-white hover:-translate-y-1 "
-            }
-            onClick={generarPalabra}
-          >
-            Volver a jugar
-          </button>
-        </div>
-      ) : (
-        <div className="bg-gray-200 p-8 rounded-lg shadow-2xl w-80 max-w-full">
-          <h1 className="text-gray-800 text-2xl text-center font-bold mb-4">¡Bienvenido al Wordle!</h1>
-
-          <div className="flex justify-center mb-4">
-            {vidasImage.map((_, index) => (
-              <img key={index} src="/img/vida.png" alt="vidas" className="w-12 h-12 mx-1" />
-            ))}
+      <div className="bg-gray-200 p-8 rounded-lg shadow-2xl w-80 max-w-full">
+        {letras.join("") === palabra ? ( // Si las letras adivinadas coinciden con la palabra completa
+          <div>
+            <p className="text-green-500 text-center text-2xl font-bold mb-2">¡Ganaste!</p>
+            <p className=" text-center text-2xl font-bold mb-3">La palabra era: {palabra}</p>
+            <button
+              className={
+                "w-full font-bold py-2 rounded-md transition-all duration-300 ease-in-out shadow-lg transform bg-blue-500 hover:bg-blue-600 text-white hover:-translate-y-1 "
+              }
+              onClick={generarPalabra}
+            >
+              Volver a jugar
+            </button>
           </div>
+        ) : (
+          <>
+            <h1 className="text-gray-800 text-2xl text-center font-bold mb-4">¡Bienvenido al Wordle!</h1>
 
-          {palabra && (
-            <>
-              {palabra}
-              <p className="text-gray-800 text-center font-semibold text-lg mb-4">
-                {letras.map((letra, index) => (
-                  <span key={index} className="mx-1">
-                    {letra}
-                  </span>
-                ))}
-              </p>
+            {palabra && (
+              <div>
+                {palabra}
+                <div className="flex justify-center mb-4">
+                  {vidasImage.map((_, index) => (
+                    <img key={index} src="/img/vida.png" alt="vidas" className="w-12 h-12 mx-1" />
+                  ))}
+                </div>
+                <p className="text-gray-800 text-center font-semibold text-lg mb-4">
+                  {letras.map((letra, index) => (
+                    <span key={index} className="mx-1">
+                      {letra}
+                    </span>
+                  ))}
+                </p>
 
-              <form onSubmit={handleSubmit} className="space-y-4">
-                <input
-                  type="text"
-                  value={inputValue}
-                  onChange={(e) => {
-                    const value = e.target.value;
-                    if (/^[a-zA-ZñÑ]?$/.test(value)) {
-                      setInputValue(value);
-                    }
-                  }}
-                  maxLength={1}
-                  className="w-full border border-gray-300 p-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all"
-                  placeholder="Ingresa una letra"
-                />
+                <form onSubmit={handleSubmit} className="space-y-4">
+                  <input
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      if (/^[a-zA-ZñÑ]?$/.test(value)) {
+                        setInputValue(value);
+                      }
+                    }}
+                    maxLength={1}
+                    className="w-full border border-gray-300 p-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all"
+                    placeholder="Ingresa una letra"
+                  />
 
-                <button
-                  type="submit"
-                  disabled={!inputValue}
-                  className={`w-full font-bold py-2 rounded-md transition-all duration-300 ease-in-out shadow-lg transform ${
-                    inputValue
-                      ? "bg-blue-500 hover:bg-blue-600 text-white hover:-translate-y-1"
-                      : "bg-gray-400 text-gray-700 cursor-not-allowed"
-                  }`}
-                >
-                  COMPARAR
-                </button>
-              </form>
-            </>
-          )}
-        </div>
-      )}
+                  <button
+                    type="submit"
+                    disabled={!inputValue}
+                    className={`w-full font-bold py-2 rounded-md transition-all duration-300 ease-in-out shadow-lg transform ${
+                      inputValue
+                        ? "bg-blue-500 hover:bg-blue-600 text-white hover:-translate-y-1"
+                        : "bg-gray-400 text-gray-700 cursor-not-allowed"
+                    }`}
+                  >
+                    COMPARAR
+                  </button>
+                </form>
+              </div>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
