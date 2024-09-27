e762ad960e505c0e2275dd826d7aa8f18ef48833
Manuel Krivoy <krivoymanuel@gmail.com>
2024-09-27 13:36:06 -0300

    finalizo primer juego- ahorcado

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import { palabras } from "./palabras.js";
 import { useEffect, useState } from "react";
+import FinalScreen from "./ahorcado/FinalScreen";
 
 function App() {
   const [palabra, setPalabra] = useState<string>("");
   const [inputValue, setInputValue] = useState<string>("");
   const [vidas, setVidas] = useState<number>(0);
   const [letras, setLetras] = useState<string[]>([]);
-
-  const vidasImage = Array.from({ length: vidas });
+  const [usedLetters, setUsedLetters] = useState<string[]>([]);
 
   const generarPalabra = () => {
     const randomIndex = Math.floor(Math.random() * palabras.length);
     const nuevaPalabra = palabras[randomIndex].toUpperCase();
     setPalabra(nuevaPalabra);
-    setLetras(Array(nuevaPalabra.length).fill("_")); // Ahora usas nuevaPalabra para generar letras
-    console.log(letras);
+    setLetras(Array(nuevaPalabra.length).fill("_"));
+    setVidas(6);
+    setUsedLetters([]);
   };
 
   useEffect(() => {
     generarPalabra();
-    setVidas(5);
   }, []);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -30,52 +30,44 @@ function App() {
         if (palabra[index] === letra) {
           return letra;
         }
-
         return letraPalabra;
       });
-
       setLetras(newLetras);
     } else {
       setVidas((prevVidas) => prevVidas - 1);
-      alert("¡Incorrecto!");
-      if (vidas === 1) {
-        alert("¡Perdiste!");
-        generarPalabra();
-        setVidas(5);
-      }
+      setUsedLetters((prevUsedLetters) => [...prevUsedLetters, letra]);
     }
-    setInputValue(""); // Limpiar el input después de cada intento
+    setInputValue("");
+  };
+
+  const renderVidas = () => {
+    return (
+      <div className="flex justify-center mb-4">
+        <img
+          src={`./img/${vidas}.png`}
+          alt="vidas"
+          className="w-36 h-36 transition-opacity"
+          style={{ opacity: vidas === 0 ? 0 : 1 }} // Para suavizar el cambio
+        />
+      </div>
+    );
   };
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-purple-500 to-blue-500">
       <div className="bg-gray-200 p-8 rounded-lg shadow-2xl w-80 max-w-full">
-        {letras.join("") === palabra ? ( // Si las letras adivinadas coinciden con la palabra completa
-          <div>
-            <p className="text-green-500 text-center text-2xl font-bold mb-2">¡Ganaste!</p>
-            <p className=" text-center text-2xl font-bold mb-3">La palabra era: {palabra}</p>
-            <button
-              className={
-                "w-full font-bold py-2 rounded-md transition-all duration-300 ease-in-out shadow-lg transform bg-blue-500 hover:bg-blue-600 text-white hover:-translate-y-1 "
-              }
-              onClick={generarPalabra}
-            >
-              Volver a jugar
-            </button>
-          </div>
+        {letras.join("") === palabra ? (
+          <FinalScreen text="GANASTE" color="green" palabra={palabra} generarPalabra={generarPalabra} />
+        ) : vidas === 0 ? (
+          <FinalScreen text="PERDISTE" color="red" palabra={palabra} generarPalabra={generarPalabra} />
         ) : (
           <>
-            <h1 className="text-gray-800 text-2xl text-center font-bold mb-4">¡Bienvenido al Wordle!</h1>
+            <h1 className="text-gray-800 text-2xl text-center font-bold mb-4">¡Ahorcado!</h1>
 
             {palabra && (
               <div>
-                {palabra}
-                <div className="flex justify-center mb-4">
-                  {vidasImage.map((_, index) => (
-                    <img key={index} src="/img/vida.png" alt="vidas" className="w-12 h-12 mx-1" />
-                  ))}
-                </div>
                 <p className="text-gray-800 text-center font-semibold text-lg mb-4">
+                  {renderVidas()}
                   {letras.map((letra, index) => (
                     <span key={index} className="mx-1">
                       {letra}
@@ -83,6 +75,7 @@ function App() {
                   ))}
                 </p>
 
+                {/* Formulario de entrada */}
                 <form onSubmit={handleSubmit} className="space-y-4">
                   <input
                     type="text"
@@ -97,6 +90,7 @@ function App() {
                     className="w-full border border-gray-300 p-2 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all"
                     placeholder="Ingresa una letra"
                   />
+                  <p>Letras incorrectas: {usedLetters.join(", ")}</p>
 
                   <button
                     type="submit"
---
+++ src/ahorcado/FinalScreen.jsx
@@ -0,0 +1,21 @@
+const FinalScreen = ({ text, color, palabra, generarPalabra }) => {
+  const colors = {
+    green: "text-green-500",
+    red: "text-red-700",
+  };
+
+  return (
+    <div>
+      <p className={`text-center text-2xl font-bold mb-2 ${colors[color]}`}>{text}</p>
+      <p className="text-center text-2xl font-bold mb-3">La palabra era: {palabra}</p>
+      <button
+        className="w-full font-bold py-2 rounded-md transition-all duration-300 ease-in-out shadow-lg transform bg-blue-500 hover:bg-blue-600 text-white hover:-translate-y-1"
+        onClick={generarPalabra}
+      >
+        Volver a jugar
+      </button>
+    </div>
+  );
+};
+
+export default FinalScreen;
